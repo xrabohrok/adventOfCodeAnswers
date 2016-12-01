f754019f3974aeda45774298b68f8f65201ac0b6
Mr. Black <westin.breger@asynchrony.com>
2016-12-01 16:14:54 -0600

    day 1, part 2 patch

--- day1/mapper.py
+++ day1/mapper.py
@@ -1,23 +1,32 @@
 cardinals = ["north", "east", "south", "west"]
 facing = 0
 dist = [0,0]
+logger = []
+dest = None
 
 
 def goDirection(direction, steps):
-    global dist
+    global dist, logger, dest
     xd = 0
     yd = 0
     if direction == "north":
-        yd = steps
+        yd = 1
     elif direction == "south":
-        yd = -steps
+        yd = -1
     elif direction == "east":
-        xd = steps
+        xd = 1
     elif direction == "west":
-        xd = -steps
+        xd = -1
 
-    dist[0] += xd
-    dist[1] += yd
+    for i in range(0,steps) :
+        dist[0] += xd
+        dist[1] += yd
+        here = "{},{}".format(dist[0],dist[1])
+        print here
+        if haveIBeenHere(here):
+            if dest == None:
+                dest = [dist[0],dist[1]]
+
 
 def turnLeft(steps):
     global facing, cardinals
@@ -34,15 +43,27 @@ def turnRight(steps):
     goDirection(cardinals[facing], steps)
 
 def runMap(commands):
+    global dist
     for command in commands:
         if command[0].upper() == 'R' :
-            turnRight(int(command[1:]))
+            if(turnRight(int(command[1:]))):
+                print "==>{}".format(abs(dist[0]) + abs(dist[1]))
         elif command[0].upper() == 'L':
-            turnLeft(int(command[1:]))
+            if(turnLeft(int(command[1:]))):
+                print "==>{}".format(abs(dist[0]) + abs(dist[1]))
         else:
             print "ERROR: something is wrong with command: " + command
 
     return abs(dist[0]) + abs(dist[1])
+
+def haveIBeenHere(here):
+    global logger
+    for log in logger:
+        if log == here:
+            return True
+
+    logger.append(here)
+    return False
 
 raw = raw_input("Enter the directions in the form of <turn><steps>*space*|<enter> (R1 L3)\n")
 s="".join(i for i in raw if (i.isdigit() or i in " rRlL"))
@@ -51,3 +72,6 @@ commands = filter(lambda a: a != None and a != '' and a != ',', rawCommands)
 
 distance = runMap(commands)
 print "The shortest path is {}, going sideways {}, and vertically {}".format(distance, dist[0], dist[1])
+
+print dest
+print "==>{}".format(abs(dest[0]) + abs(dest[1]))
